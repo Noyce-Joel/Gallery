3790c187eebc118bd439b399e2a9ce57e57bca55
Joel Noyce <noyce.joel@gmail.com>
2024-01-11 22:41:03 +0000

    local files

--- src/app/components/AddToAlbum.tsx
+++ src/app/components/AddToAlbum.tsx
@@ -41,7 +41,7 @@ export default function AddToAlbum({
       setUploaded(true);
       setSelected([]);
       router.refresh()
-    }, 2000);
+    }, 1500);
 
     if (albumName === "") {
       const selectedAlbums = albums.filter((album) => {
