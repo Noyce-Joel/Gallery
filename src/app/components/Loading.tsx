bea4c0f9503a0b43fa356e10e74f70ce37a3a34f
Joel Noyce <noyce.joel@gmail.com>
2024-01-24 00:26:45 +0000

    performance check

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,19 +1,33 @@
-import React, { useContext } from 'react'
-import { ThemeContext, ThemeContextProps } from '../context/Context';
+import React, { useContext } from "react";
+import { ThemeContext, ThemeContextProps } from "../context/Context";
 
 function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Loading() {
-  const {theme} = useContext(ThemeContext) as ThemeContextProps;  
-  
+  const { theme } = useContext(ThemeContext) as ThemeContextProps;
 
   return (
     <>
-    <div className={classNames('loading', theme === 'dark' ? 'bg-[#dddbcb]' : 'bg-gray-800')} ></div>
-    <div className={classNames('loading', theme === 'dark' ? 'bg-[#dddbcb]' : 'bg-gray-800')} ></div>
-    <div className={classNames('loading', theme === 'dark' ? 'bg-[#dddbcb]' : 'bg-gray-800')} ></div>
+      <div
+        className={classNames(
+          "loading",
+          theme === "dark" ? "bg-[#dddbcb]" : "bg-gray-800"
+        )}
+      ></div>
+      <div
+        className={classNames(
+          "loading",
+          theme === "dark" ? "bg-[#dddbcb]" : "bg-gray-800"
+        )}
+      ></div>
+      <div
+        className={classNames(
+          "loading",
+          theme === "dark" ? "bg-[#dddbcb]" : "bg-gray-800"
+        )}
+      ></div>
     </>
-  )
+  );
 }
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -12,7 +12,6 @@ import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { useContext, useState } from "react";
 import { ThemeContext, ThemeContextProps } from "../context/Context";
-import { text } from "stream/consumers";
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
@@ -34,11 +33,16 @@ export default function Nav({
   const { data: session, status } = useSession();
   const [openSignOut, setOpenSignOut] = useState<boolean>(false);
   const { theme } = useContext(ThemeContext) as ThemeContextProps;
-  console.log(theme)
-  const textColor = theme === "dark" ? "text-white" : "text-gray-800";  
-  const drkNav = theme === "dark" ? "bg-[#dddbcb] text-gray-800 " : "bg-gray-800 text-white";
-  const drkBorder = theme === 'dark' ?  'border-white' : 'border-gray-800';
-  const drkHover = theme === 'dark' ? 'hover:bg-[#dddbcb] hover:text-gray-800' : 'hover:bg-gray-800 hover:text-white'; 
+  console.log(theme);
+  const textColor = theme === "dark" ? "text-white" : "text-gray-800";
+  const drkMode = theme === "dark" ? "bg-gray-800 " : "bg-[#dddbcb] ";
+  const drkNav =
+    theme === "dark" ? "bg-[#dddbcb] text-gray-800 " : "bg-gray-800 text-white";
+  const drkBorder = theme === "dark" ? "border-white" : "border-gray-800";
+  const drkHover =
+    theme === "dark"
+      ? "hover:bg-[#dddbcb] hover:text-gray-800"
+      : "hover:bg-gray-800 hover:text-white";
   const pathname = usePathname();
   const navigation = [
     {
@@ -73,201 +77,107 @@ export default function Nav({
   if (status === "loading") {
     return <></>;
   }
-  // if (!session) {
-  //   return (
-  //     <div
-  //       className={`flex grow flex-col gap-y-5 overflow-y-auto color-black ${textColor} w-[225px] pr-6 `}
-  //     >
-  //       <div className={`flex h-16 shrink-0 gap-2 p-6 `}>
-  //       <h1 className={`flex font-bold whitespace-nowrap tracking-wider ${textColor}`} >
-  //         Noyce Photos
-  //       </h1>
-  //       </div>
-  //       <nav className="flex flex-1 flex-col">
-  //         <ul role="list" className="flex flex-1 flex-col gap-y-7">
-  //           <li>
-  //             <ul role="list" className="-mx-2 space-y-1">
-  //               {navigation.map((item) => (
-  //                 <li key={item.name}>
-  //                   <a
-  //                     href={item.href}
-  //                     className={classNames(
-  //                       item.current
-  //                         ? `${drkNav}`
-  //                         : `${textColor} hover:text-white hover:bg-gray-800`,
-  //                       "group flex gap-x-7 rounded-r-[100px] p-2 pl-7 text-sm leading-6 font-semibold"
-  //                     )}
-  //                   >
-  //                     <item.icon
-  //                       className="h-6 w-6 shrink-0"
-  //                       aria-hidden="true"
-  //                     />
-  //                     {item.name}
-  //                   </a>
-  //                 </li>
-  //               ))}
-  //             </ul>
-  //           </li>
-  //           <li>
-  //             <div className={`text-xs font-semibold leading-6 ${textColor} flex gap-x-3 pl-7`}>
-  //               Albums
-  //             </div>
-  //             <ul role="list" className="-mx-2 mt-2 space-y-1 overflow-y-scroll">
-  //               {albums.map((album, idx) => (
-  //                 <li key={idx}>
-  //                   <Link
-  //                     href={`/albums/${album}`}
-  //                     className={classNames(
-  //                       pathname === `/albums/${album}`
-  //                         ? `${textColor} rounded-r-[100px]`
-  //                         : "",
-  //                       "group flex gap-x-7 rounded-md p-2 text-sm leading-6 font-semibold pl-7"
-  //                     )}
-  //                   >
-  //                     <span
-  //                       className={classNames(
-  //                         pathname === `/albums/${album}`
-  //                           ? `${drkNav} border border-white`
-  //                           : "bg-gray-800 text-white group-hover:animate-bounce group-hover:text-gray-800 group-hover:bg-[#dddbcb]",
-  //                         " flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-800  text-[0.625rem] font-medium  "
-  //                       )}
-  //                     >
-  //                       {album.charAt(0).toUpperCase()}
-  //                     </span>
-  //                     <span
-  //                       className={classNames(
-  //                         pathname === `/albums/${album}` ? `${textColor}` : "",
-  //                         `${textColor} truncate`
-  //                       )}
-  //                     >
-  //                       {removeHyphens(album)}
-  //                     </span>
-  //                   </Link>
-  //                 </li>
-  //               ))}
-  //             </ul>
-  //           </li>
-  //           <li className="mt-auto -mx-2 space-y-7">
-  //             <div className="flex flex-col gap-x-7 gap-y-5 rounded-md p-2 text-sm leading-6 font-semibold pl-7">
-  //               <div className="flex gap-7">
-  //                 <CircleStackIcon className="h-6 w-6" />
-  //                 <p>Credits</p>
-  //               </div>
-  //               <div className="flex flex-col gap-y-3">
-  //                 <div className="relative w-full">
-  //                   <div className={`absolute border ${drkBorder} w-full h-[4px] rounded-xl`} />
-  //                   <div
-  //                     className={`absolute ${drkNav} h-[4px] rounded-xl w-auto`}
-  //                     style={{ width: `${usagePercentage}%` }}
-  //                   />
-  //                 </div>
-  //                 <div className="flex justify-start items-start">
-  //                   {usage.credits.usage} / {usage.credits.limit} used
-  //                 </div>
-  //               </div>
-  //             </div>
-  //           </li>
-  //         </ul>
-  //       </nav>
-  //     </div>
-  //   );
-  // }
+
   session ? console.log(session.user?.name) : null;
   return (
     <div
-    className={`flex flex-col gap-y-5 color-black ${textColor} w-[225px] pr-6 `}
-  >
-    <div className={`flex h-16 shrink-0 gap-2 p-6 `}>
-    <h1 className={`flex font-bold whitespace-nowrap tracking-wider ${textColor}`} >
-      Noyce Photos
-    </h1>
-    </div>
-    <nav className="flex flex-1 flex-col">
-      <ul role="list" className="flex flex-1 flex-col gap-y-7">
-        <li>
-          <ul role="list" className="-mx-2 space-y-1">
-            {navigation.map((item) => (
-              <li key={item.name}>
-                <a
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? `${drkNav}`
-                      : `${textColor}`,
-                    `${drkHover} group flex gap-x-7 rounded-r-[100px] p-2 pl-7 text-sm leading-6 font-semibold`
-                  )}
-                >
-                  <item.icon
-                    className="h-6 w-6 shrink-0"
-                    aria-hidden="true"
-                  />
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </li>
-        <li className="flex flex-col grow overflow-y-auto h-auto">
-          <div className={`text-xs font-semibold leading-6 ${textColor} flex gap-x-3 pl-9`}>
-            Albums
-          </div>
-          <ul role="list" className="-mx-2 mt-2 space-y-1">
-            {albums.map((album, idx) => (
-              <li key={idx}>
-                <Link
-                  href={`/albums/${album}`}
-                  className={classNames(
-                    pathname === `/albums/${album}`
-                      ? `${textColor} rounded-r-[100px]`
-                      : "",
-                    "group flex gap-x-7 rounded-md p-2 text-sm leading-6 font-semibold pl-7"
-                  )}
-                >
-                  <span
+      className={`flex flex-col gap-y-5 color-black ${textColor} ${drkMode} w-[225px] pr-6 transition duration-[0.8s] ease-in-out`}
+    >
+      <div className={`flex h-16 shrink-0 gap-2 p-6 `}>
+        <h1
+          className={`flex font-bold whitespace-nowrap tracking-wider ${textColor}`}
+        >
+          Noyce Photos
+        </h1>
+      </div>
+      <nav className="flex flex-1 flex-col">
+        <ul role="list" className="flex flex-1 flex-col gap-y-7">
+          <li>
+            <ul role="list" className="-mx-2 space-y-1">
+              {navigation.map((item) => (
+                <li key={item.name}>
+                  <a
+                    href={item.href}
+                    className={classNames(
+                      item.current ? `${drkNav}` : `${textColor}`,
+                      `${drkHover} group flex gap-x-7 rounded-r-[100px] p-2 pl-7 text-sm leading-6 font-semibold`
+                    )}
+                  >
+                    <item.icon
+                      className="h-6 w-6 shrink-0"
+                      aria-hidden="true"
+                    />
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li className="flex flex-col grow overflow-y-auto h-auto">
+            <div
+              className={`text-xs font-semibold leading-6 ${textColor} flex gap-x-3 pl-9`}
+            >
+              Albums
+            </div>
+            <ul role="list" className="-mx-2 mt-2 space-y-1">
+              {albums.map((album, idx) => (
+                <li key={idx}>
+                  <Link
+                    href={`/albums/${album}`}
                     className={classNames(
                       pathname === `/albums/${album}`
-                        ? `${drkNav} `
-                        : "bg-gray-800 border border-white text-white group-hover:animate-bounce group-hover:text-gray-800 group-hover:bg-[#dddbcb]",
-                      " flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-800  text-[0.625rem] font-medium  "
+                        ? `${textColor} rounded-r-[100px]`
+                        : "",
+                      "group flex gap-x-7 rounded-md p-2 text-sm leading-6 font-semibold pl-7"
                     )}
                   >
-                    {album.charAt(0).toUpperCase()}
-                  </span>
-                  <span
-                    className={classNames(
-                      pathname === `/albums/${album}` ? `${textColor}` : "",
-                      `${textColor} truncate`
-                    )}
-                  >
-                    {removeHyphens(album)}
-                  </span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </li>
-        <li className="mt-auto -mx-2 space-y-7">
-          <div className="flex flex-col gap-x-7 gap-y-5 rounded-md p-3 text-sm leading-6 font-semibold pl-7 pb-9">
-            <div className="flex gap-7">
-              <CircleStackIcon className="h-6 w-6" />
-              <p>Credits</p>
-            </div>
-            <div className="flex flex-col gap-y-3">
-              <div className="relative w-full">
-                <div className={`absolute border ${drkBorder} w-full h-[4px] rounded-xl`} />
-                <div
-                  className={`absolute ${drkNav} h-[4px] rounded-xl w-auto`}
-                  style={{ width: `${usagePercentage}%` }}
-                />
+                    <span
+                      className={classNames(
+                        pathname === `/albums/${album}`
+                          ? `${drkNav} `
+                          : "bg-gray-800 border border-white text-white group-hover:animate-bounce group-hover:text-gray-800 group-hover:bg-[#dddbcb]",
+                        " flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-800  text-[0.625rem] font-medium  "
+                      )}
+                    >
+                      {album.charAt(0).toUpperCase()}
+                    </span>
+                    <span
+                      className={classNames(
+                        pathname === `/albums/${album}` ? `${textColor}` : "",
+                        `${textColor} truncate`
+                      )}
+                    >
+                      {removeHyphens(album)}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li className="mt-auto -mx-2 space-y-7">
+            <div className="flex flex-col gap-x-7 gap-y-5 rounded-md p-3 text-sm leading-6 font-semibold pl-7 pb-9">
+              <div className="flex gap-7">
+                <CircleStackIcon className="h-6 w-6" />
+                <p>Credits</p>
               </div>
-              <div className="flex justify-start items-start">
-                {usage.credits.usage} / {usage.credits.limit} used
+              <div className="flex flex-col gap-y-3">
+                <div className="relative w-full">
+                  <div
+                    className={`absolute border ${drkBorder} w-full h-[4px] rounded-xl`}
+                  />
+                  <div
+                    className={`absolute ${drkNav} h-[4px] rounded-xl w-auto`}
+                    style={{ width: `${usagePercentage}%` }}
+                  />
+                </div>
+                <div className="flex justify-start items-start">
+                  {usage.credits.usage} / {usage.credits.limit} used
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-      </ul>
-    </nav>
-  </div>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
