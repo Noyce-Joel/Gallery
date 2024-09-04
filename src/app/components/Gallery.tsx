d1d37dcde4b43e1e1ddec54d9ce50079207e15c4
Joel Noyce <noyce.joel@gmail.com>
2024-09-04 03:42:20 +0100

    update

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -207,9 +207,12 @@ function Gallery({ results }: { results: { resources: SearchResult[] } }) {
       />
       <button
         onClick={handleNavigateToProjects}
-        className="fixed bottom-4 right-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded z-50"
+        className={
+            
+            `absolute top-[25px] right-24 bg-gray-800 hover:bg-[#dddbcb] text-white  hover:text-gray-800 rounded-xl flex-nowrap whitespace-nowrap flex group-hover gap-3 p-2 px-3 text-sm shadow-sm`
+        }
       >
-        View as Project
+        3D
       </button>
 
       {addToAlbumDialogue ? (
