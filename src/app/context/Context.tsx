acef3e80d807913cc8593a81c35afc62d9257bcd
Joel Noyce <noyce.joel@gmail.com>
2024-11-22 12:22:50 +0000

    store theme in local storage to avoid being revalidated

--- src/app/context/Context.tsx
+++ src/app/context/Context.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { Dispatch, SetStateAction, createContext, useState } from 'react'
+import React, { Dispatch, SetStateAction, createContext, useEffect, useState } from 'react'
 
 export interface ThemeContextProps {
 	theme: string
@@ -15,7 +15,14 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
 	const currentHour = new Date().getHours()
 	const isDayTime = currentHour >= 5 && currentHour < 14
 	const [theme, setTheme] = useState<string>(isDayTime ? 'light' : 'dark')
+	useEffect(() => {
+		const storedTheme = localStorage.getItem('theme')
+		if (storedTheme) {
+			setTheme(storedTheme)
+		}
 
+		localStorage.setItem('theme', theme)
+	}, [theme])
 	return (
 		<ThemeContext.Provider value={{ theme, setTheme }}>
 			{children}
