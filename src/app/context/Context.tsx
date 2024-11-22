8e6184e0a71ffee564070996cbf1b0e9c21695ad
Joel Noyce <noyce.joel@gmail.com>
2024-11-22 12:33:33 +0000

    default theme set in local storage

--- src/app/context/Context.tsx
+++ src/app/context/Context.tsx
@@ -1,6 +1,12 @@
 'use client'
 
-import React, { Dispatch, SetStateAction, createContext, useState } from 'react'
+import React, {
+	Dispatch,
+	SetStateAction,
+	createContext,
+	useEffect,
+	useState,
+} from 'react'
 
 export interface ThemeContextProps {
 	theme: string
@@ -12,10 +18,23 @@ export const ThemeContext = createContext<ThemeContextProps | undefined>(
 )
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-	const currentHour = new Date().getHours()
-	const isDayTime = currentHour >= 5 && currentHour < 14
-	const [theme, setTheme] = useState<string>('dark')
-	
+	const [theme, setTheme] = useState<string>('light')
+
+	useEffect(() => {
+		const savedTheme = localStorage.getItem('theme')
+		if (savedTheme) {
+			setTheme(savedTheme)
+		} else {
+			const currentHour = new Date().getHours()
+			const isDayTime = currentHour >= 5 && currentHour < 19
+			setTheme(isDayTime ? 'light' : 'dark')
+		}
+	}, [])
+
+	useEffect(() => {
+		localStorage.setItem('theme', theme)
+	}, [theme])
+
 	return (
 		<ThemeContext.Provider value={{ theme, setTheme }}>
 			{children}
